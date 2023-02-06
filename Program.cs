a84296bf9dc634ecc6d6b713f35347e0222c2cc1
Mikko Keinänen <mikko.keinanen@gmail.com>
2023-02-06 23:57:15 +0200

    Register db context

--- Program.cs
+++ Program.cs
@@ -1,27 +1,19 @@
 using System.Net;
 using System.Security.Cryptography.X509Certificates;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services
+    .AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-/*builder.WebHost.ConfigureKestrel((context, serverOptions) =>
-{
-    var certPem = File.ReadAllText("/home/mikko/localhost+1.pem");
-    var eccPem = File.ReadAllText("/home/mikko/localhost+1-key.pem");
-
-    var cert = X509Certificate2.CreateFromPem(certPem, eccPem);
-
-    serverOptions.Listen(IPAddress.Any, 5054, listenoptions => {
-        listenoptions.UseHttps(cert);
-    });
-
-});*/
 
 var app = builder.Build();
 
