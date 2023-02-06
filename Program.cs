3a656b9d5cffcaf88ae9de58adfdf8660ebc244a
Mikko Keinänen <mikko.keinanen@gmail.com>
2023-02-06 22:12:05 +0200

    initial loading of development certificate

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.WebHost.ConfigureKestrel((context, serverOptions) =>
+{
+    var certPem = File.ReadAllText("/home/mikko/localhost+1.pem");
+    var eccPem = File.ReadAllText("/home/mikko/localhost+1-key.pem");
+
+    var cert = X509Certificate2.CreateFromPem(certPem, eccPem);
+
+    serverOptions.Listen(IPAddress.Any, 5054, listenoptions => {
+        listenoptions.UseHttps(cert);
+    });
+
+});
 
 var app = builder.Build();
 
