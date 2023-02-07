1296aa8c86fa295e9eb13f84fc8dd07bd746cd88
Mikko Keinänen <mikko.keinanen@gmail.com>
2023-02-07 20:20:45 +0200

    Fix POST location header

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -19,7 +19,7 @@ public class TodoController : ControllerBase
     {
         _context.TodoItems.Add(todoItem);
         await _context.SaveChangesAsync();
-        return CreatedAtAction(nameof(PostTodoItem), new { id = todoItem.Id }, todoItem);
+        return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
     }
 
     [HttpGet("{id}")]
