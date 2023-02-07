dc399bf9f6151e331fb4fd53a6b43eb71cb27bf0
Mikko Keinänen <mikko.keinanen@gmail.com>
2023-02-07 20:41:28 +0200

    set TodoItems required

--- Models/TodoContext.cs
+++ Models/TodoContext.cs
@@ -7,5 +7,5 @@ public class TodoContext : DbContext
     public TodoContext(DbContextOptions<TodoContext> options)
         : base(options) { }
 
-    public DbSet<TodoItem> TodoItems { get; set; } = null;
+    public required DbSet<TodoItem> TodoItems { get; set; }
 }
